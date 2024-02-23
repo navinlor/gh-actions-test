b7c8ded92253469d613b98cfe67d13953cabd4a9
ivankorol <ivan.korol@dataart.com>
2024-02-23 11:35:08 +0200

    update

--- GitHubActionsTest/obj/Debug/net8.0/GitHubActionsTest.AssemblyInfo.cs
+++ GitHubActionsTest/obj/Debug/net8.0/GitHubActionsTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitHubActionsTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08ee098533f7aaa1173a0211d695c3a683376f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitHubActionsTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitHubActionsTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
